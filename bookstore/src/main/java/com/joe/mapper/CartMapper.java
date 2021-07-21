48047bc1766b7724567397558c8db4192c7831f6
joe <wwwkkpop0218@gmail.com>
2021-07-21 16:13:37 +0800

    Dao

---
+++ bookstore/src/main/java/com/joe/mapper/CartMapper.java
@@ -0,0 +1,16 @@
+package com.joe.mapper;
+
+import com.joe.entity.Cart;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author joe
+ * @since 2021-07-14
+ */
+public interface CartMapper extends BaseMapper<Cart> {
+
+}
---
+++ bookstore/src/main/java/com/joe/mapper/OrdersDetailMapper.java
@@ -0,0 +1,16 @@
+package com.joe.mapper;
+
+import com.joe.entity.OrdersDetail;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author joe
+ * @since 2021-07-14
+ */
+public interface OrdersDetailMapper extends BaseMapper<OrdersDetail> {
+
+}
