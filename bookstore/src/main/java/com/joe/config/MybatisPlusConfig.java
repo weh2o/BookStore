56768deb7d91f105e717879ff5e4c6c5263ff8db
joe <wwwkkpop0218@gmail.com>
2021-07-21 16:16:40 +0800

    分頁配置

---
+++ bookstore/src/main/java/com/joe/config/MybatisPlusConfig.java
@@ -0,0 +1,18 @@
+package com.joe.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class MybatisPlusConfig {
+
+    //分頁攔截器
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+}
