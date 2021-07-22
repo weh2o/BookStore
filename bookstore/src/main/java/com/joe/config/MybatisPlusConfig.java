dfc13e5227b23c687aa121cae132d49bc10ae1bb
joe <wwwkkpop0218@gmail.com>
2021-07-22 15:16:41 +0800

    分頁攔截器

--- bookstore/src/main/java/com/joe/config/MybatisPlusConfig.java
+++ bookstore/src/main/java/com/joe/config/MybatisPlusConfig.java
@@ -1,7 +1,6 @@
 package com.joe.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
